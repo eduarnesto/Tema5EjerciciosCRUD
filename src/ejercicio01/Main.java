802d6212a3169b52f69cffacb9c07caf2434e7f7
eduarnesto <eduardo.arnesto@iesnervion.es>
2024-03-06 10:15:49 +0100

    asd

--- src/ejercicio01/Main.java
+++ src/ejercicio01/Main.java
@@ -8,6 +8,9 @@ public class Main {
 		//Array para guardar 30 alumnos
 		Alumno[] alumnos = new Alumno[30];
 		
+		//Variable para guardar la opcion que elija el usuario
+		int opc;
+		
 		//Boolean para salir del bucle
 		boolean salir = false;
 
@@ -16,11 +19,27 @@ public class Main {
 		
 		//Repito las intrucciones hasta que el usuario quiera salir
 		while (salir) {
+			//Se muestra el menu
+			menu();
 			
+			//Leo del teclado
+			opc = sc.nextInt();
+			
+			//Depende de la opcion elegida se ejecuta 
+			switch (opc) {
+			case 1 -> System.out.println();
+			
+			}
 		}
+		
+		//Muestro mensaje para notificar al usuario que ha salido del programa
+		System.out.println("Has salido del programa");
+		
+		//Cierro el Scanner
+		sc.close();
 	}
 	
-	public void menu() {
+	public static void menu() {
 		System.out.println("----------");
 		System.out.println("1. Listado");
 		System.out.println("2. Nuevo alumno");
