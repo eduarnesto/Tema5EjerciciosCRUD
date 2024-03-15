ec49f3c3e25b7bbe4d3ddae21607868ceff2e97f
eduarnesto <eduardo.arnesto@iesnervion.es>
2024-03-15 09:19:17 +0100

    sdjfjsdjsdf

--- src/ejercicios/ejercicio02/Gestisimal.java
+++ src/ejercicios/ejercicio02/Gestisimal.java
@@ -6,68 +6,112 @@ public class Gestisimal {
 
 	public static boolean alta(Articulo art) {
 		boolean anyadido = false;
-		
+
 		// 1º Comprobar si el artículo ya existe
 		int i = 0;
 		int posEncontrado = buscaArticulo(art.getCodigo());
 
 		// Si no he encontrado el artículo lo tengo que añadir
-		if(posEncontrado<articulos.length) {
-			
-			while(i<articulos.length && articulos[i]!=null) {
+		if (posEncontrado >= 0) {
+
+			while (i < articulos.length && articulos[i] != null) {
 				i++;
 			}
-			
-			if(i<articulos.length) {
+
+			if (i < articulos.length) {
 				articulos[i] = art;
 				anyadido = true;
 			}
 		}
-		
+
 		return anyadido;
 	}
-	
+
 	private static int buscaArticulo(int codigo) {
 		int i = 0;
-		boolean enc = false;
+		int pos = -1;
 
-		while (i < articulos.length && !enc) {
+		while (i < articulos.length && pos == -1) {
 			// Compruebo que la posición no sea null
 			// Compruebo si el artículo de la posición i es igual a art (artículo pasado por
 			// parámetro)
-			if (articulos[i] != null && articulos[i].getCodigo()==codigo) {
-				enc = true;
+			if (articulos[i] != null && articulos[i].getCodigo() == codigo) {
+				pos = i;
 			}
 
 			i++;
 		}
-		
+
 		return i;
 	}
-	
-	public static boolean eliminaArticulo (int codigo) {
+
+	public static boolean eliminaArticulo(int codigo) {
 		boolean eliminado = false;
-		int i = buscaArticulo(codigo);
-		
-		if(i<articulos.length) {
-			articulos[i] = null;
+		int pos = buscaArticulo(codigo);
+
+		if (pos >= 0) {
+			articulos[pos] = null;
 			eliminado = true;
 		}
-		
+
 		return eliminado;
 	}
-	
-	public static boolean modificaArticulo (int codigo, float precio) {
+
+	public static boolean modificaDescripcion(int codigo, String descripcion) {
 		boolean modificado = false;
-		int i = buscaArticulo(codigo);
-		
-		if(i<articulos.length) {
-			articulos[i].setPrecioVenta(precio);
+		int pos = buscaArticulo(codigo);
+
+		if (pos >= 0) {
+			articulos[pos].setDescripcion(descripcion);
 			modificado = true;
 		}
-		
+
+		return modificado;
+	}
+	
+	public static boolean modificaPrecioCompra(int codigo, float precio) {
+		boolean modificado = false;
+		int pos = buscaArticulo(codigo);
+
+		if (pos >= 0) {
+			articulos[pos].setPrecioCompra(precio);
+			modificado = true;
+		}
+
+		return modificado;
+	}
+	
+	public static boolean modificaPrecioVenta(int codigo, float precio) {
+		boolean modificado = false;
+		int pos = buscaArticulo(codigo);
+
+		if (pos >= 0) {
+			articulos[pos].setPrecioVenta(precio);
+			modificado = true;
+		}
+
 		return modificado;
 	}
 
+	public static void entradaArticulo(int codigo, int cantidad) {
+
+		int pos = buscaArticulo(codigo);
+		cantidad += articulos[pos].getStock();
+		articulos[pos].setStock(cantidad);
+
+	}
+
+	public static boolean salidaArticulo(int codigo, int cantidad) {
+		boolean salida = false;
+		
+		int pos = buscaArticulo(codigo);
+		cantidad -= articulos[pos].getStock();
+		if (cantidad >= 0) {
+			articulos[pos].setStock(cantidad);
+			salida = true;
+		}
+
+		return salida;
+	}
 
 }
--- src/ejercicios/ejercicio02/Principal.java
+++ src/ejercicios/ejercicio02/Principal.java
@@ -11,6 +11,9 @@ public class Principal {
 		// Variable para guardar la opcion que elija el usuario
 		int opc;
 
+		// Variable para guardar la opcion al elegir el campo que quieras modificar
+		int opcMod;
+		
 		// Bucle que se recorre siempre hasta que presionemos la opcion 7
 		do {
 			// Llamo al menu
@@ -22,12 +25,27 @@ public class Principal {
 
 			// Realizo la operacion que me ha marcado el usurio
 			switch (opc) {
-			case 1 -> System.out.println("Saliendo...");
-			case 2 -> crearArticulo();
-			case 3 -> borrarArticulo();
-			case 4 -> modificarArticulo();
-			case 5 -> Syso;
-			case 6 -> Syso;
+			case 1:
+				listado();
+				break;
+			case 2:
+				crearArticulo();
+				break;
+			case 3:
+				borrarArticulo();
+				break;
+			case 4:
+				menuModificacion();
+				opcMod = sc.nextInt();
+				sc.nextLine();
+				modificarArticulo(opcMod);
+				break;
+			case 5:
+				anyadirStock();
+				break;
+			case 6:
+				retirarStock();
+				break;
 			}
 		} while (opc != 7);
 
@@ -49,6 +67,38 @@ public class Principal {
 		System.out.println("Elige una opcion");
 	}
 
+	public static void menuModificacion() {
+		System.out.println("1. Descripcion");
+		System.out.println("2. Precio de compra");
+		System.out.println("3. Precio de venta");
+		System.out.println("Elija el campo que desee modificar");
+	}
+
+	private static void listado() {
+		for (int i = 0; i < Gestisimal.articulos.length; i++) {
+			if (Gestisimal.articulos[i] != null) {
+				System.out.println(i + ". " + Gestisimal.articulos[i].toString());
+				System.out.println();
+			}
+		}
+	}
+
+	private static int preguntaCodigo() {
+		int codigo;
+		System.out.println("Introduzca el codigo del articulo que quieres modificar");
+		codigo = sc.nextInt();
+		sc.nextLine();
+		return codigo;
+	}
+
+	private static int preguntaCantidad() {
+		int cantidad;
+		System.out.println("Introduzca la cantidad deseada");
+		cantidad = sc.nextInt();
+		sc.nextLine();
+		return cantidad;
+	}
+
 	public static void crearArticulo() {
 		int codigo;
 		String descripcion;
@@ -60,61 +110,108 @@ public class Principal {
 		System.out.println("Introduzca el codigo del articulo");
 		codigo = sc.nextInt();
 		sc.nextLine();
-		
+
 		System.out.println("Introduzca la descripcion del articulo");
 		descripcion = sc.nextLine();
-		
+
 		System.out.println("Introduzca el precio al que ha sido comprado el articulo");
 		precioCompra = sc.nextFloat();
 		sc.nextLine();
-		
+
 		System.out.println("Introduzca el precio al que se vende el articulo");
 		precioVenta = sc.nextFloat();
 		sc.nextLine();
-		
+
 		System.out.println("Introduzca el stock del articulo");
 		stock = sc.nextInt();
 		sc.nextLine();
-		
+
 		art = new Articulo(codigo, descripcion, precioCompra, precioVenta, stock);
-		
+
 		if (Gestisimal.alta(art)) {
 			System.out.println("Se ha añdido el articulo correctamente");
 		} else {
 			System.out.println("No se ha podido añadir el articulo");
 		}
 	}
-	
+
 	public static void borrarArticulo() {
 		int codigo;
-		
-		System.out.println("Introduzca el codigo del articulo que quieres eliminar");
-		codigo = sc.nextInt();
-		sc.nextLine();
-		
+
+		codigo = preguntaCodigo();
+
 		if (Gestisimal.eliminaArticulo(codigo)) {
 			System.out.println("El articulo se ha eliminado correctamente");
 		} else {
 			System.out.println("El articulo no se ha podido eliminar");
 		}
 	}
-	
-	public static void modificarArticulo() {
+
+	public static void modificarArticulo(int opc) {
+		String descripcion;
 		float precio;
 		int codigo;
+
+		codigo = preguntaCodigo();
+
+		switch (opc) {
+		case 1:
+			System.out.println("Introduzca la nueva descipcion del articulo que quieras modificar");
+			descripcion = sc.nextLine();
+			if (Gestisimal.modificaDescripcion(codigo, descripcion)) {
+				System.out.println("El articulo se ha modificado correctamente");
+			} else {
+				System.out.println("El articulo no se ha modificado eliminar");
+			}
+			break;
+		case 2:
+			System.out.println("Introduzca el nuevo precio de compra del articulo que quieres modificar");
+			precio = sc.nextFloat();
+			sc.nextLine();
+			if (Gestisimal.modificaPrecioCompra(codigo, precio)) {
+				System.out.println("El articulo se ha modificado correctamente");
+			} else {
+				System.out.println("El articulo no se ha modificado eliminar");
+			}
+			break;
+		case 3:
+			System.out.println("Introduzca el nuevo precio de venta del articulo que quieres modificar");
+			precio = sc.nextFloat();
+			sc.nextLine();
+			if (Gestisimal.modificaPrecioVenta(codigo, precio)) {
+				System.out.println("El articulo se ha modificado correctamente");
+			} else {
+				System.out.println("El articulo no se ha modificado eliminar");
+			}
+			break;
+		default:
+			break;
+		}
 		
-		System.out.println("Introduzca el codigo del articulo que quieres modificar");
-		codigo = sc.nextInt();
-		sc.nextLine();
-		
-		System.out.println("Introduzca el nuevo precio del articulo que quieres modificar");
-		precio = sc.nextFloat();
-		sc.nextLine();
-		
-		if (Gestisimal.modificaArticulo(codigo, precio)) {
-			System.out.println("El articulo se ha modificado correctamente");
+	}
+
+	public static void anyadirStock() {
+		int codigo;
+		int cantidad;
+
+		codigo = preguntaCodigo();
+		cantidad = preguntaCantidad();
+
+		Gestisimal.entradaArticulo(codigo, cantidad);
+		System.out.println("El stock del articulo se ha modificado correctamente");
+	}
+
+	public static void retirarStock() {
+		int codigo;
+		int cantidad;
+
+		codigo = preguntaCodigo();
+		cantidad = preguntaCantidad();
+
+		if (Gestisimal.salidaArticulo(codigo, cantidad)) {
+			System.out.println("El stock del articulo se ha modificado correctamente");
 		} else {
-			System.out.println("El articulo no se ha modificado eliminar");
+			System.out.println("No se ha podido modificar el stock del articulo");
 		}
 	}
 }
